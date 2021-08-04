09ceb17d21afa3c9dbab774716ccf005d39c5253
Marvin Maina <machariamarvin625@gmail.com>
2021-08-04 16:08:40 +0300

    Add menu navigation bar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 $(function () {
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 80) {
-            $(".nav-bar").removeAttr("id");
-            $(".nav-bar").addClass("navbar-scroll").css("margin-top", "-20px");
-        } else($(".nav-bar").removeClass("navbar-scroll"))
+            if ($(this).scrollTop() > 80) {
+                $(".nav-bar").removeAttr("id");
+                $(".nav-bar").addClass("navbar-scroll").css("margin-top", "-20px");
+            } else {
+                $(".nav-bar").removeClass("navbar-scroll")
+        }
+    })
+
+    $("#menu-bar").click(function(){
+        $(".menu").slideDown();
+        $(".nav-nav-brand").removeAttr("id");
+        $(".nav-nav-brand").addClass("menu-nav-brand");
+        $(".menu-cancel").fadeIn();
+        $(".body-overlay").fadeIn();
+    })
+
+    $(".menu-cancel").click(function(){
+        $(".menu").slideUp();
+        $(".nav-nav-brand").removeClass("menu-nav-brand");
+        $(".nav-nav-brand").attr("id", "navbar-brand");
+        $(".menu-cancel").fadeOut();
+        $(".body-overlay").fadeOut();
     })
 })
