c873eca7de5caab06c5a94115a6f4624069d1dd7
Marvin Maina <machariamarvin625@gmail.com>
2021-08-04 13:05:19 +0300

    Modify indentation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-$(function(){
-   $(window).scroll(function(){
-       if ($(this).scrollTop() > 80){
-        $(".nav-bar").removeAttr("id");
-           $(".nav-bar").addClass("navbar-scroll").css("margin-top", "-20px");
-       }
-
-       else ($(".nav-bar").removeClass("navbar-scroll"))
-   })
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 80) {
+            $(".nav-bar").removeAttr("id");
+            $(".nav-bar").addClass("navbar-scroll").css("margin-top", "-20px");
+        } else($(".nav-bar").removeClass("navbar-scroll"))
+    })
 })
