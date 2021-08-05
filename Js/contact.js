877ef409b9c206d18dba816c954af839de4bbee0
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-08-05 11:42:34 +0300

    validating subscribe form

--- Js/contact.js
+++ Js/contact.js
@@ -22,6 +22,32 @@ function validate(){
     alert('Dear '+username+'\n  your message has been received.')
 }
 
+//SUBSCRIBE form validation
+
+function subcribe(){
+    var username=document.getElementById("name2").value;
+    var email=document.getElementById("email2").value;
+    //var radio=document.getElementById("select").value;
+
+    if(username==''){
+        alert('enter your name');
+        username.focus();
+        return false;
+        
+    }else if(email==''){
+        alert('enter your email address');
+        email.focus();
+        return false
+    }else if(!document.getElementById("daily").checked && !document.getElementById("weekly").checked 
+    && !document.getElementById("monthly").checked) {
+        alert('select duration')
+        message.focus()
+        return false;
+        
+    }else
+    alert('Dear '+username+'\n  your have subscribed to receive updates.')
+}
+
 //hiding and showing subscribe form
 $(document).ready(function(){
     $(".click").click(function(event){
