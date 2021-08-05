cd5a6855f7a683c1131dfa617cc4f54970213a27
lanarokip <lankipkoecharo@gmail.com>
2021-08-05 12:58:02 +0300

    formarted the code so as to have a perfect identation

--- javascript/about.js
+++ javascript/about.js
@@ -1,31 +1,25 @@
-
-$(function(){
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 80){
-         $(".nav-bar").removeAttr("id");
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 80) {
+            $(".nav-bar").removeAttr("id");
             $(".nav-bar").addClass("navbar-scroll").css("margin-top", "-20px");
-        }
-        else ($(".nav-bar").removeClass("navbar-scroll"))
+        } else($(".nav-bar").removeClass("navbar-scroll"))
     })
- })
- $("#menu-bar").click(function(){
-    $(".menu").slideDown();
-    $(".nav-nav-brand").removeAttr("id");
-    $(".nav-nav-brand").addClass("menu-nav-brand");
-    $(".nav-bar-brand").fadeOut();
-    $(".menu-cancel").fadeIn();
-    $(".body-overlay").fadeIn();
+
+    $("#menu-bar").click(function () {
+        $(".menu").slideDown();
+        $(".nav-nav-brand").removeAttr("id");
+        $(".nav-nav-brand").addClass("menu-nav-brand");
+        $(".nav-bar-brand").fadeOut();
+        $(".menu-cancel").fadeIn();
+        $(".body-overlay").fadeIn();
+    })
+    $(".menu-cancel").click(function () {
+        $(".menu").slideUp();
+        $(".nav-nav-brand").removeClass("menu-nav-brand");
+        $(".nav-nav-brand").attr("id", "navbar-brand");
+        $(".nav-bar-brand").fadeIn();
+        $(".menu-cancel").fadeOut();
+        $(".body-overlay").fadeOut();
+    })
 })
-$(".menu-cancel").click(function(){
-    $(".menu").slideUp();
-    $(".nav-nav-brand").removeClass("menu-nav-brand");
-    $(".nav-nav-brand").attr("id", "navbar-brand");
-    $(".nav-bar-brand").fadeIn();
-    $(".menu-cancel").fadeOut();
-    $(".body-overlay").fadeOut();
-})
-
-
-
-
-
